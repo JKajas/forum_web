457a41a0e4000e43c0bb2f90fcaf25b5bf054756
JKajas <jakub.kajas@gmail.com>
2022-09-14 00:00:52 +0200

    feat: Add new validators, placeholders and labels to display

--- forum_comments/forms.py
+++ forum_comments/forms.py
@@ -1,36 +1,126 @@
 from django import forms
-from forum_comments.models import Article, UserModel, Comment
+from django.conf import settings
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
+from forum_comments import validators
+
+from forum_comments.models import Article, Comment, UserModel
 
 
-class Login(forms.Form):
-    username = forms.CharField(max_length= 30)
-    password = forms.CharField(max_length=30, widget=forms.PasswordInput)
-
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=30,
+        label="Email address",
+        widget=forms.TextInput(attrs={"placeholder": "email@example.com"}),
+    )
+    password = forms.CharField(
+        max_length=30,
+        label="Password",
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"}),
+    )
+    user = None
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+        user = authenticate(
+        email=username,
+        password=password,
+        )
+        if user is None:
+            raise ValidationError("Data is not correct. Check your email and password")
+        self.user = user
 
 class formArticle(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'text']
+        fields = ["title", "text", "image"]
+        labels = {"text": "", "title": "Title:"}
+        widgets = {
+            "title": forms.TextInput(
+                attrs={"placeholder": "Enter your article's title"}
+            )
+        }
 
 
 class registerUserForm(forms.ModelForm):
-    password1 = forms.CharField(max_length=150, widget=forms.PasswordInput)
-    password2 = forms.CharField(max_length=150, widget=forms.PasswordInput)
+    password = forms.CharField(
+        max_length=150,
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Enter your password"}
+        ),
+    )
+    password_repeat = forms.CharField(
+        max_length=150,
+        label="Repeat Password",
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Repeat your password"}
+        ),
+    )
+
     class Meta:
         model = UserModel
-        fields = ['username', 'email']
+        fields = ["username", "email"]
+        labels = {"username": "Username", "email": "Email"}
+        widgets = {
+            "username": forms.TextInput(
+                attrs={"placeholder": "Enter your username"}
+            ),
+            "email": forms.TextInput(
+                attrs={"placeholder": "Enter your email"}
+            ),
+        }
 
-
-class ResetPassUser(forms.Form):
-    email = forms.CharField(max_length=150)
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_repeat = cleaned_data.get("password_repeat")
+        if password != password_repeat:
+           raise ValidationError("Passwords should be the same!")
+        validators.password_validator(password)
 
 
 class ResetPassword(forms.Form):
-    password = forms.CharField(max_length=150)
-    password_repeat = forms.CharField(max_length=150)
+    email = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={"placeholder": "Enter your email"}),
+    )
 
+
+class NewPassword(forms.Form):
+    password = forms.CharField(
+        max_length=150,
+        label="New password",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Enter your new password here"}
+        ),
+    )
+    password_repeat = forms.CharField(
+        max_length=150,
+        label="Repeat new password",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Repeat your new password"}
+        ),
+    )
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_repeat = cleaned_data.get("password_repeat")
+        if password != password_repeat:
+            raise ValidationError("Passwords should be the same", code="invalid")
+        with open(settings.BASE_DIR / "leaked_passwords.txt","r") as leaked_password:
+            if password in leaked_password.read():
+                raise ValidationError("Your password is too popular")
+        validators.password_validator(password)
 
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["text"].widget.attrs.update(
+            {"placeholder": "Write your comment here"}
+        )
+
     class Meta:
         model = Comment
         fields = ["text"]
@@ -38,6 +128,7 @@ class CommentForm(forms.ModelForm):
 
 class CreateUserAdmin(forms.ModelForm):
     password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+
     class Meta:
         model = UserModel
-        fields = ['username', 'email']
+        fields = ["username", "email"]
--- forum_comments/validators.py
+++ forum_comments/validators.py
@@ -1,22 +1,37 @@
+import os
+
 from django.core.exceptions import ValidationError
 
-'''
+"""
 Validators checking NIP and domain
-'''
+"""
+
 
 def validate_NIP(NIP):
     NIP_list = list(map(int, NIP))
-    ctr_w = [6,5,7,2,3,4,5,6,7]
-    if len(NIP)<10:
+    ctr_w = [6, 5, 7, 2, 3, 4, 5, 6, 7]
+    if len(NIP) < 10:
         raise ValidationError("NIP is too short!")
-    elif len(NIP)>10:
+    elif len(NIP) > 10:
         raise ValidationError("NIP is too long!")
-    elif sum([x*y for x, y in zip(NIP_list, ctr_w)])%11 != NIP_list[9]:
-        raise ValidationError('Wrong number!')
-    else: print('its ok')
+    elif sum([x * y for x, y in zip(NIP_list, ctr_w)]) % 11 != NIP_list[9]:
+        raise ValidationError("Wrong number!")
 
 
 def validate_domain(domain):
-    if not 'www.' or not '.com' or not '.pl' in domain:
-        raise ValidationError('Wrong domain!')
+    if not "www." or not ".com" or not ".pl" in domain:
+        raise ValidationError("Wrong domain!")
 
+
+def len_title_valid(object):
+    if len(object) < 10:
+        raise ValidationError("Your title should have at least 10 characters!")
+
+
+def password_validator(password):
+    if len(password) < 8:
+        raise ValidationError(
+            "Your password should have at least 8 characters"
+        )
+    if not any(char.isdigit() for char in password):
+        raise ValidationError("Password should have at least one digit")

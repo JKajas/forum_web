1189b766ab9bc11fa9a2300ac63b1594b1e5beb6
JKajas <jakub.kajas@gmail.com>
2022-09-14 00:03:19 +0200

    feat: Add support for query string bar by middleware

--- forum_comments/middleware.py
+++ forum_comments/middleware.py
@@ -1,21 +1,23 @@
 from django.shortcuts import redirect
-from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.http import urlencode
+
 from . import models
 
-'''
+"""
 Middleware fixes problem with url with any user in database 
 Example:
 Url patern: /profile/<username> 
-If localhost:8000/profile/exuser but logged user has username: userexample
+If url address is: localhost:8000/profile/exuser, but logged user has username: userexample
 and exuser is in database you can reach that endpoint
 Middleware changes argument <username> on correct logged username
-'''
+"""
 
 
 class CheckCompatibility:
     def __init__(self, get_response):
         self.get_response = get_response
+
     def __call__(self, request):
         response = self.get_response(request)
         return response
@@ -23,27 +25,54 @@ class CheckCompatibility:
     def process_view(self, request, view_func, view_args, view_kwargs):
         try:
             if request.user.is_authenticated:
-                if request.user.username != view_kwargs['username']:  
-                    return redirect(reverse(view_func, kwargs={'username':request.user.username}))
+                if request.user.username != view_kwargs["username"]:
+                    return redirect(
+                        reverse(
+                            view_func,
+                            kwargs={"username": request.user.username},
+                        )
+                    )
         except:
-            return None   
+            return None
 
 
-'''
-Middleware check if token is in database
-'''           
+# Middleware check if token is in database
+
 
 class CheckToken:
     def __init__(self, get_response):
         self.get_response = get_response
+
     def __call__(self, request):
         response = self.get_response(request)
         return response
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         try:
-            if models.PasswordToken.objects.filter(token=view_kwargs['url_token']).exists():
+            if not models.PasswordToken.objects.filter(
+                token=view_kwargs["url_token"]
+            ).exists():
+                return redirect("index")
+            return None
+        except:
+            None
+
+
+class SearchQuery:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        try:
+            if not request.GET["title"]:
                 return None
-            else:
-                return redirect('index')
+            if not request.path == reverse("index"):
+                return redirect(
+                    reverse("index") + "?title=" + request.GET["title"]
+                )
         except:
             None

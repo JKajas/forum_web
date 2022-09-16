7a3c983c59b36d3b177f165d2fe58626a56ee13d
JKajas <jakub.kajas@gmail.com>
2022-09-17 00:16:29 +0200

    feat: Add support for images and plugin to write articles

--- forum/settings.py
+++ forum/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import socket
 from pathlib import Path
-import socket 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a@!l&1d^c^$l0w0ep7va1jbj$z@2f0%el!mfq-j3zv5dt^s*q)'
+SECRET_KEY = (
+    "django-insecure-a@!l&1d^c^$l0w0ep7va1jbj$z@2f0%el!mfq-j3zv5dt^s*q)"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,80 +31,89 @@ DEBUG = True
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
-ALLOWED_HOSTS = [ip,]
+ALLOWED_HOSTS = [
+    ip,
+]
 
 
 AUTHENTICATION_BACKENDS = [
-    'forum_comments.backends.UserBackend',
-    'django.contrib.auth.backends.ModelBackend',
-    ]
+    "forum_comments.backends.UserBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
 
-AUTH_USER_MODEL = 'forum_comments.UserModel'
+AUTH_USER_MODEL = "forum_comments.UserModel"
 
 
-EMAIL_BACKEND = 'forum_comments.backends.NewEmailBackend'
-EMAIL_FILE_PATH = 'emails'
+EMAIL_BACKEND = "forum_comments.backends.NewEmailBackend"
+EMAIL_FILE_PATH = "emails"
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',  
-    'django.contrib.staticfiles',
-    'forum_comments',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "forum_comments",
+    "ckeditor",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'forum_comments.middleware.CheckCompatibility',
-    'forum_comments.middleware.CheckToken',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "forum_comments.middleware.CheckCompatibility",
+    "forum_comments.middleware.CheckToken",
+    "forum_comments.middleware.SearchQuery",
 ]
 
-ROOT_URLCONF = 'forum.urls'
+ROOT_URLCONF = "forum.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "template/"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "template/"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'forum.wsgi.application'
+WSGI_APPLICATION = "forum.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("MYSQL_DATABASE") ,
-        'HOST': "db",
-        'PORT': "3306",
-        'USER': os.environ.get("MYSQL_USER"),
-        'PASSWORD' : os.environ.get("MYSQL_ROOT_PASSWORD"),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("MYSQL_DATABASE"),
+        "HOST": "db",
+        "PORT": "3306",
+        "USER": os.environ.get("MYSQL_USER"),
+        "PASSWORD": os.environ.get("MYSQL_ROOT_PASSWORD"),
     }
 }
 
-CELERY_BROKER_URL = 'amqp://guest:guest@broker:5672//'
-CELERY_RESULT_BACKEND = 'rpc://'
-CELERY_IMPORTS = ['forum_comments.tasks']
+CELERY_BROKER_URL = "amqp://guest:guest@broker:5672//"
+CELERY_RESULT_BACKEND = "rpc://"
+CELERY_IMPORTS = ["forum_comments.tasks"]
 CELERY_CELERYBEAT_SCHEDULE = []
 
 
@@ -111,16 +122,16 @@ CELERY_CELERYBEAT_SCHEDULE = []
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -128,9 +139,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = "pl"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -140,11 +151,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'forum/static_files/'
+STATIC_URL = os.path.join(BASE_DIR, "forum_comments/static_files/")
+STATIC_ROOT = os.path.join(BASE_DIR, "forum_comments/static_files/")
+CKEDITOR_BASEPATH = os.path.join(
+    BASE_DIR, "forum_comments/static_files/ckeditor/ckeditor/"
+)
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_IMAGE_BACKEND = "pillow"
+# Media files
+
+MEDIA_URL = os.path.join(BASE_DIR, "forum_comments/media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "forum_comments/media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "index"
--- forum_comments/models.py
+++ forum_comments/models.py
@@ -1,144 +1,168 @@
 from datetime import datetime
-from wsgiref import validate
+
+from ckeditor.fields import RichTextField
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    Permission,
+    PermissionsMixin,
+)
 from django.core.validators import validate_email
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Permission
-from django.conf import settings
-from django.contrib import admin
-from forum_comments.validators import validate_NIP, validate_domain
+
+from forum_comments import validators
+
+# UserManager creates superuser with permissions only to manage usermodel
 
 
-'''
-UserManager creates superuser with permissions only to manage usermodel
-'''
 class UserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         if not email:
-            raise ValueError('User must have an email address')
-        user = self.model(
-            email=self.normalize_email(email),
-            username = username
-        )
+            raise ValueError("User must have an email address")
+        user = self.model(email=self.normalize_email(email), username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
     def create_superuser(self, email, username, password=None):
         user = self.create_user(
-            email=email,
-            username=username,
-            password=password
-        ) 
-        user.is_staff = True 
-        permission = Permission.objects.all().filter(content_type_id = 6)
+            email=email, username=username, password=password
+        )
+        user.is_staff = True
+        permission = Permission.objects.all().filter(content_type_id=6)
         perm_list = list(permission)
         user.user_permissions.set(perm_list)
         user.save(using=self._db)
         return user
 
-'''
-OrganizationManager creates superuser only to manage organizationmodel
-'''
+
+# OrganizationManager creates superuser only to manage organizationmodel
+
 
 class OrganizationManager(BaseUserManager):
-    
     def create_user(self, email, username, password=None):
         print(email)
         if not email:
-            raise ValueError('User must have an email address')
-        user = self.model(
-            email=self.normalize_email(email),
-            username = username
-        )
+            raise ValueError("User must have an email address")
+        user = self.model(email=self.normalize_email(email), username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
     def create_superuser(self, email, username, password=None):
         user = self.create_user(
-            email=email,
-            username=username,
-            password=password
-        ) 
-        user.is_staff = True 
-        permission = Permission.objects.all().filter(content_type_id = 8)
+            email=email, username=username, password=password
+        )
+        user.is_staff = True
+        permission = Permission.objects.all().filter(content_type_id=8)
         perm_list = list(permission)
         user.user_permissions.set(perm_list)
         user.save(using=self._db)
-        return user 
+        return user
 
 
 class OrganizationModel(models.Model):
     name = models.CharField(max_length=30)
-    NIP = models.CharField(max_length=10, validators= [validate_NIP,])    
+    NIP = models.CharField(
+        max_length=10,
+        validators=[
+            validators.validate_NIP,
+        ],
+    )
     country = models.CharField(max_length=30)
-    domain_name = models.CharField(max_length=30, validators=[validate_domain,])
-    objects= OrganizationManager()
+    domain_name = models.CharField(
+        max_length=30,
+        validators=[
+            validators.validate_domain,
+        ],
+    )
+    objects = OrganizationManager()
 
     def __str__(self):
-        return self.name    
+        return self.name
 
 
-'''
-UserModel inherits from PermissionsMixin for has user.permissions attribute
-'''
-class UserModel(AbstractBaseUser,PermissionsMixin):
-    organization = models.ForeignKey(OrganizationModel, blank=True, null=True, on_delete=models.CASCADE) 
-    username = models.CharField(max_length=30)
-    email = models.EmailField(max_length=30,unique=True, validators= [validate_email])
+# UserModel inherits from PermissionsMixin for has user.permissions attribute
+
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
+    organization = models.ForeignKey(
+        OrganizationModel, blank=True, null=True, on_delete=models.CASCADE
+    )
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        error_messages={
+            "unique": "User with that username already exists.",
+        },
+    )
+    email = models.EmailField(
+        max_length=30,
+        unique=True,
+        validators=[validate_email],
+        error_messages={"unique": "User with that email already exists."},
+    )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS =  ["username", "password"]
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "password"]
 
-    def __str__ (self):
+    def __str__(self):
         return self.username
 
-'''
-Proxy model created to has other user manager(OrganizationManager)
-who create user with other permissions  
-'''
+
+# Proxy model created to has other user manager(OrganizationManager)
+# who create user with other permissions
+
 
 class OrgUserModel(UserModel):
-    objects= OrganizationManager()
+    objects = OrganizationManager()
+
     class Meta:
-        proxy= True
+        proxy = True
 
 
 class Article(models.Model):
     date = models.DateField(auto_now=True)
-    title = models.CharField(max_length= 30)
-    text = models.CharField(max_length=15000)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=False, blank=False)
+    time = models.DateTimeField(auto_now=True)
+    title = models.CharField(
+        max_length=30,
+        validators=[
+            validators.len_title_valid,
+        ],
+    )
+    text = RichTextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
     url = models.CharField(max_length=17, unique=True)
-    
+
 
 class Comment(models.Model):
     date = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
-
-
 
 
 class PasswordToken(models.Model):
     date = models.DateTimeField(auto_now=True)
     token = models.CharField(max_length=150, unique=True)
-    username= models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    
-    '''
-    PasswordToken has classmethod check_token managed by celery in tasks.py 
-    '''
+    username = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+    # PasswordToken has classmethod check_token managed by celery in tasks.py
 
     @classmethod
     def check_token(cls):
         objs = cls.objects.all()
         for obj in objs:
-            time = (datetime.now()- obj.date.replace(tzinfo=None))
+            time = datetime.now() - obj.date.replace(tzinfo=None)
             if time.total_seconds() > 60:
                 obj.delete()
                 return True
- 
-
-
